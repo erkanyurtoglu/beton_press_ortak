98a635cfc0c309f14518a0ca24a6c1d2c23c25b9
erkanyurtoglu <158731690+erkanyurtoglu@users.noreply.github.com>
2025-05-15 12:26:34 +0300

    deneme

--- btn/BetonPressTest/BetonPressTest/Form1.Designer.cs
+++ btn/BetonPressTest/BetonPressTest/Form1.Designer.cs
@@ -28,7 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(650, 47);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "qwewqeq";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // Form1
             // 
@@ -37,12 +47,16 @@
             this.BackgroundImage = global::BetonPressTest.Properties.Resources.LİYA__2_;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1006, 553);
+            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- btn/BetonPressTest/BetonPressTest/Form1.cs
+++ btn/BetonPressTest/BetonPressTest/Form1.cs
@@ -16,5 +16,10 @@ namespace BetonPressTest
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
